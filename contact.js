ac2362f12640bd65c06817695a9614c914476492
Skylaaaaaaaa <136422357+Skylaaaaaaaa@users.noreply.github.com>
2023-06-13 14:27:56 +0800

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,11 @@
+a= document.getElementById('mail');
+a.setAttribute("href", "https://mail.google.com/mail/u/3/#inbox");
+
+a= document.getElementById('fb');
+a.setAttribute("href", "https://www.facebook.com/Alyana.kylec");
+
+a= document.getElementById('ig');
+a.setAttribute("href", "https://www.instagram.com/_alyanakyle/?hl=en");
+
+a= document.getElementById('twitter');
+a.setAttribute("href", "https://twitter.com/vtorongg");
